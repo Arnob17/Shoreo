5aa2475d041a096a897b4dec3280bfe7bd462e28
arnob17 <arnob01611@gmail.com>
2025-01-13 01:38:59 +0600

    socket.2

--- socketio.js
+++ socketio.js
@@ -10,16 +10,51 @@ function live_olympiad(app, knex, io) {
     //         io.emit('user_joined', user);
     //     })
     // });
+    let questions = [
+        {
+            title: 'Who is the current president of USA?',
+            options: {
+                a: 'Donald Trump',
+                b: 'Joe Biden',
+                c: 'Tom Cruise',
+                d: 'Arnob Rahman'
+            },
+            description: 'Few days ago, a new election occured, who is the new president?'
+        },
+        {
+            title: 'Who is the current president of USA2?',
+            options: {
+                a: 'Donald Trump',
+                b: 'Joe Biden',
+                c: 'Tom Cruise',
+                d: 'Arnob Rahman'
+            },
+            description: 'Few days ago, a new election occured, who is the new president?'
+        },
+        {
+            title: 'Who is the current president of USA3?',
+            options: {
+                a: 'Donald Trump',
+                b: 'Joe Biden',
+                c: 'Tom Cruise',
+                d: 'Arnob Rahman'
+            },
+            description: 'Few days ago, a new election occured, who is the new president?',
+            correct_answer: 'a'
+        },
+    ]
     io.on('connection', (socket) => {
-        socket.on('joinRoom', ({ room, username }) => {
+        socket.on('joinRoom', async ({ room, username, role }) => {
+            let user = await knex('users').where({token: username});
             socket.join(room);
         
             // Add user to the room
             if (!rooms[room]) rooms[room] = [];
-            rooms[room].push({ id: socket.id, username });
+            rooms[room].push({ id: socket.id, user: user[0], role: role });
         
             // Notify other users in the room
-            socket.to(room).emit('userConnected', { id: socket.id, username });
+            socket.to(room).emit('userConnected', { id: socket.id, user: user[0], role: role});
+            // socket.to(room).emit('userConnected', rooms[room]);
         
             // Send the list of users in the room to the new user
             io.to(socket.id).emit('roomUsers', rooms[room]);
@@ -37,6 +72,14 @@ function live_olympiad(app, knex, io) {
         
             console.log(`User disconnected: ${socket.id}`);
         });
+        let i = 0;
+        socket.on('check', (role, state) => {
+            socket.emit('point', (role, state='true'?11:-11));
+            i=i+1;
+            socket.emit('question', questions[i]);
+        })
+        console.log(i);
+        socket.emit('question', questions[i]);
     })
 }
 
